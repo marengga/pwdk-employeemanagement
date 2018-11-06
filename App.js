ec9ec8eb9543b87472cff2eb93ba28970d20dcd5
Andri K. Marengga <andree.marengga@gmail.com>
2018-11-06 19:49:44 +0700

    Created main app scheme, added basic reducers

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { View, Text } from 'react-native';
+import { Header } from './src/components/common';
+import reducers from './src/reducers';
 
 export default class App extends React.Component {
   render() {
+    const store = createStore(reducers);
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <View>
+          <Header headerText="Please Login" />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
